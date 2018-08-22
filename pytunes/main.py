f62794cd0e5f404b6597708eaa12dd43a5816863
EC2 Default User <ec2-user@ip-172-31-26-56.eu-west-1.compute.internal>
2018-08-22 18:14:20 +0000

    Created project selteton

---
+++ pytunes/main.py
@@ -0,0 +1,18 @@
+"""Script to consolidate two music libraries"""
+import sys
+
+def main():
+    """Main entry point for the script."""
+    print("Program ran!!!")
+    pass
+  
+def get_songs_from_library(path):
+    """Return a list of tuples containing the songs found in a library."""
+    pass
+  
+def get_song_metadata(path):
+    """Return a list of tuples containing the songs found in a library."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
